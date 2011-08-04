f079519048fc23895d755090655aed3f4a39da14
Mijail Cisneros <misha@spb.com>
2011-08-04 15:28:27 +0400

    Initial commit

---
+++ bash-shortcuts.sh
@@ -0,0 +1,12 @@
+# cd
+alias .='cd ..'
+alias ..='cd ../..'
+
+# clear
+alias cl='clear'
+
+# ls
+alias ls="ls -F"
+alias l="ls -lAh"
+alias ll="ls -l"
+alias la='ls -A'
---
+++ git.sh
@@ -0,0 +1,12 @@
+_dir=$dir
+dir=`dirname $BASH_SOURCE`
+
+. $dir/git-completion.sh
+. $dir/git-shortcuts.sh
+. $dir/bash-shortcuts.sh
+
+cd /d/Code
+
+export PS1="\[\033[0;33m\]\$(__git_ps1 '%s ')\[\033[00m\]$PS1"
+
+dir=$_dir
