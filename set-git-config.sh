f3b7625f12c75609e3ce371e74926487cf26b32a
Mijail Cisneros <m@spbtv.com>
2012-03-05 17:04:24 +0400

    Add comments

--- set-git-config.sh
+++ set-git-config.sh
@@ -7,28 +7,39 @@ if [ "$1" = "" ]; then
 	fi
 	exit 1
 fi
+
+# Identity #
+############
 git config --global --unset-all user.name
-git config --global user.name "Mijail Cisneros"
+git config --global user.name $1
 git config --global --unset-all user.email
 git config --global user.email $2
 
 # Color #
+########
 git config --global color.branch auto
 git config --global color.diff auto
 git config --global color.interactive auto
 git config --global color.status auto
 
 # Core #
+########
 git config --global core.excludesfile $PWD/.gitexcludes
 
 # Push Default #
-#git config --global push.default nothing
-#git config --global push.default tracking
+################
+# nothing do not push anything. 
+# matching push all matching branches. All branches having the same name in both ends are considered to be matching. This is the default. 
+# tracking push the current branch to its upstream branch. 
+# current push the current branch to a branch of the same name.
 git config --global push.default current
 
 # Branch #
+##########
 git config --global branch.autosetuprebase always
 
+# Show result #
+###############
 git config --global --list
 
 exit 0
