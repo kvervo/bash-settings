6bb22dac56fe9ddfdeb80b542a72771e1ccfa1ce
Mijail Cisneros <m@spbtv.com>
2012-03-15 15:10:35 +0400

    Push and Pull now include origin remote name, so can only call gush and gull. This is only possible when setting global config push.default to current value

--- git-shortcuts.sh
+++ git-shortcuts.sh
@@ -7,39 +7,51 @@ alias gcwtf='msg="`curl http://whatthecommit.com/index.txt 2>/dev/null`"; git co
 alias gcm='git commit -m'
 alias gca='git commit -a -m'
 
-alias gcl='git clone'
+# Git Status
 alias gs='git status -s'
 alias gss='git status -s; echo "## Submodules ##";git submodule status'
+
+# Git usual workflow commands
+alias gcl='git clone'
 alias ga='git add'
-alias gull='git pull'
-alias gush='git push --recurse-submodules=check'
-alias gushc='git push --recurse-submodules=check'
+alias gull='git pull origin'
+alias gush='git push --recurse-submodules=check origin'
 alias gf='git fetch'
-#alias gm='git merge'
 alias gm='git merge --no-ff'
 alias gsh='git show'
+
+# Git log
 alias gl='git log --decorate --stat -C'
-alias gk='gitk'
-alias gka='gitk --all'
 alias gll='git show -s --pretty="tformat:%h (%s, %ad)" --date=short'
 alias glg='git log --decorate --graph --pretty="format:%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit --date=relative'
 alias gwho="sh -c 'git log -i -1 --pretty=\"format:%an <%ae>\" --author=\"$1\"' -"
+
+# Gitk
+alias gk='gitk'
+alias gka='gitk --all'
+
+# Git branch
 alias gb='git branch'
 alias gba='git branch -a'
 alias gbr='git ls-remote'
 alias gco='git checkout'
-alias gr='git remote -v'
-alias gra='git remote add'
 alias gdel='git branch -d'
 alias gnew='git checkout -b'
+
+# Git Remote
+alias gr='git remote -v'
+alias gra='git remote add'
+
+# Git Diff
 alias gd='git diff'
 alias gd1='echo "git diff HEAD";  git diff HEAD'
 alias gd2='echo "git diff HEAD^"; git diff HEAD^'
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias grmall="git status | grep 'deleted:' | awk '{print \$3}' | xargs git rm -f"
-alias gups='git reset --hard'
+
+# Git Stash
 alias gst='git stash --include-untracked'
 alias gstp='git stash pop'
+
+# Git Tag
 alias gt='git tag'
 
 # Git Configuration
@@ -72,3 +84,8 @@ alias svnsi='git svn show-ignore'
 alias svnl='git svn log'
 alias svnc='git svn dcommit'
 alias svncl='git svn clone'
+
+# Other
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias grmall="git status | grep 'deleted:' | awk '{print \$3}' | xargs git rm -f"
+alias gups='git reset --hard'
