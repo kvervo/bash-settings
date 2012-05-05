25c8665c85ef53a56084ef51afb67b7fb63835fd
Mijail Cisneros <m@spbtv.com>
2012-05-05 12:19:21 +0400

    Add alias for local configuration. Usage: gconfiglocal NAME EMAIL

--- bash-shortcuts.sh
+++ bash-shortcuts.sh
@@ -11,3 +11,23 @@ alias ls="ls -F"
 alias l="ls -lAh"
 alias ll="ls -l"
 alias la='ls -A'
+
+
+
+function gconfiglocal { 
+if [ "$1" = "" ]; then
+	echo "Empty name parameter"
+	if [ "$2" = "" ]; then
+		echo "Empty email parameter"
+		exit 1
+	fi
+	exit 1
+fi
+
+# Identity #
+############
+git config --local --unset-all user.name
+git config --local user.name $1
+git config --local --unset-all user.email
+git config --local user.email $2
+}
