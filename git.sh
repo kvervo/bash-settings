8625c8600f01a0c42f593842509c237a0e93bc5b
kvervo <cisneros@mijail.ru>
2012-08-13 19:08:28 +0400

    Add comments to code

--- git.sh
+++ git.sh
@@ -1,12 +1,13 @@
+# Execute all shortcuts files
 _dir=$dir
 dir=`dirname $BASH_SOURCE`
 
 . $dir/git-shortcuts.sh
 . $dir/git-completion.sh
-. $dir/code-shortcuts.sh
-. $dir/bash-shortcuts.sh
+. $dir/code-shortcuts.sh # personal bash script. This line should be deleted
+. $dir/bash-shortcuts.sh 
 
-
+# This is the start path for git bash
 cd /d/Code
 
 export PS1="\[\033[0;33m\]\$(__git_ps1 '%s ')\[\033[00m\]$PS1"
